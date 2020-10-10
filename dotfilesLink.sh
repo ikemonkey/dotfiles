8a05260b4d9d379a8d3278a21e76651a400fe89a
ikemonkey <hasijun@gmail.com>
2020-10-10 04:47:48 +0000

    mod

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,4 @@
 
 ln -sf $DOTFILEPATH/.vimrc $HOME/.vimrc
 ln -sf $DOTFILEPATH/.gvimrc $HOME/.gvimrc
-ln -sf $DOTFILEPATH/colors $HOME/.vim
-ln -sf $DOTFILEPATH/autoload $HOME/.vim
-ln -sf $DOTFILEPATH/bitmaps $HOME/.vim
+ln -sf $DOTFILEPATH/.vim $HOME/.vim
